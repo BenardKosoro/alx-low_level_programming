443e6d8c7f37201d83fdc6bbf700befac457f1a8
BenardKosoro <benard.osoro@gmail.com>
2023-02-17 06:23:53 -0800

    updated copy

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,13 +7,14 @@
  *
  */
 int main(void)
+
 {
 	int ones = '0';
 	int tens = '0';
 
-	for (tens = '0'; tens <= '9'; tens++)/* prints tens digits */
+	for (tens = '0'; tens <= '9'; tens++)/* prints tens digits*/
 	{
-		for (ones = '0'; tens <= '9'; ones++)/*prints ones digits */
+		for (ones = '0'; ones <= '9'; ones++)/*prints ones digits*/
 		{
 			if (!((ones == tens) || (tens > ones)))/* eliminates repitation */
 			{
