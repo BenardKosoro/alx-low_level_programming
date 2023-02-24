cbfc722e669f6e8d444e42aafa5695f46857e44e
BenardKosoro <benard.osoro@gmail.com>
2023-02-24 03:48:00 -0800

    9-fizz_buzz.c updated

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,7 +13,7 @@ int main(void)
 
 	for (x = 1; x <= 100; x++)
 	{
-		if ((x % 3 == 8) && (x % 5 == 0))
+		if ((x % 3 == 0) && (x % 5 == 0))
 		{
 			printf("FizzBuzz");
 		}
@@ -34,6 +34,6 @@ int main(void)
 			printf(" ");
 		}
 	}
-	printf('\n');
+	printf("\n");
 	return (0);
 }
