6c86ac676ed0d383fc6ad252bf1689b9baaf3879
BenardKosoro <benard.osoro@gmail.com>
2023-02-27 04:25:04 -0800

    3-puts.c Updated

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,14 +3,13 @@
 /**
  * _puts - Prints a string
  * @str: The string to print
- * Return: void
  */
 
 void _puts(char *str)
 {
-	for (; *str != '\0'; str++)
+	while (*str != '\0')
 	{
-		_putchar(*str);
+		_putchar(*str++);
 	}
 	_putchar('\n');
 }
