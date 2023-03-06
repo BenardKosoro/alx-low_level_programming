1107f041478f992751c589f1c82689eab394b486
BenardKosoro <benard.osoro@gmail.com>
2023-03-06 06:01:33 -0800

    8-print_diagsums.c update

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -17,7 +17,7 @@ void print_diagsums(int *a, int size)
 
 	for (y = 0; y < size; y++)
 	{
-		sum1 = sum2 + a[y * size + y];
+		sum1 = sum1 + a[y * size + y];
 	}
 	for (y = size - 1; y >= 0; y--)
 	{
