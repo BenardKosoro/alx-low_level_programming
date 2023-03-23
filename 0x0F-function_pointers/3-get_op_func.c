c7659b6d84372cec4dc3792b10ff381dbe162e40
BenardKosoro <benard.osoro@gmail.com>
2023-03-22 22:48:53 -0700

    3-get_op_func.c updated

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -9,11 +9,8 @@
  * Description: struction with operators
  * Return: pointer to operator and function
  */
-
 int (*get_op_func(char *s))(int, int)
 {
-	int i = 0;
-
 	op_t ops[] = {
 		{"+", op_add},
 		{"-", op_sub},
@@ -22,12 +19,16 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
+	int i = 0;
 
-	while (ops[i].op != NULL)
+	while (ops[i].op)
 	{
-		if (s == ops[i].op)
+		if (*s == *ops[i].op)
+		{
 			return (ops[i].f);
+		}
 		i++;
 	}
-
+	printf("Error\n");
+	exit(99);
 }
