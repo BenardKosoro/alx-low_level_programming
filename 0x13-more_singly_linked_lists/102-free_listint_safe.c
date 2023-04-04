8bb6f304cb0f8c0994f271460029d331f2738803
BenardKosoro <benard.osoro@gmail.com>
2023-04-04 04:53:05 -0700

    Updated

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,30 +1,47 @@
 #include "lists.h"
 
 /**
- * free_listint_safe - prints a list
- * @h: address of pointer to first node
+ * free_listint_safe - Frees a listint_t list safely (ie.
+ * can free lists containing loops)
  *
- * Return: address of head
+ * @h: A pointer to the address of
+ * the head of the listint_t list.
+ *
+ * Return: The size of the list that was freed.
+ *
+ * Description: The function sets the head to NULL.
  */
+
 size_t free_listint_safe(listint_t **h)
 {
-	size_t c = 0;
-	listint_t *head, *temp;
-	long diff;
+	listint_t *tmp;
+	size_t nodes, index;
 
-	if (!h)
-		return (0);
-	head = *h;
-	*h = NULL;
-	while (head)
+	nodes = looped_listint_count(*h);
+
+	if (nodes == 0)
 	{
-		c++;
-		diff = head->next - head;
-		temp = head;
-		free(temp);
-		if (diff >= 0)
-			break;
-		head = head->next;
+		for (; h != NULL && *h != NULL; nodes++)
+		{
+			tmp = (*h)->next;
+			free(*h);
+			*h = tmp;
+		}
 	}
-	return (c);
+
+	else
+	{
+		for (index = 0; index < nodes; index++)
+		{
+			tmp = (*h)->next;
+			free(*h);
+			*h = tmp;
+		}
+
+		*h = NULL;
+	}
+
+	h = NULL;
+
+	return (nodes);
 }
