600f30c490ab4d6925ce914e84161794ba5f8030
BenardKosoro <benard.osoro@gmail.com>
2023-04-07 23:49:10 -0700

    chngeas to main header files

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,34 +1,37 @@
+#include "main.h"
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include "main.h"
 
 /**
- * create_file - Create `filename' with content from `text_content'
- * @filename: name of file to create
- * @text_content: content to insert into file `filename'
+ * create_file - Creates a file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
  *
- * Return: 1 on success, -1 if error occurs using `write`.
+ * Return: If the function fails - -1.
+ *         Otherwise - 1.
  */
-
 int create_file(const char *filename, char *text_content)
 {
-	size_t i = 0;
-	ssize_t w = 0;
-	int fd;
+	int op, wr, len = 0;
 
 	if (filename == NULL)
 		return (-1);
-	for (i = 0; text_content && text_content[i]; i++)
-		;
-	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, S_IRUSR | S_IWUSR);
-	if (fd == -1)
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	wr = write(op, text_content, len);
+
+	if (op == -1 || wr == -1)
 		return (-1);
-	if (i)
-		w = write(fd, text_content, i);
-	close(fd);
-	if (w == -1)
-		return (-1);
+
+	close(op);
+
 	return (1);
 }
